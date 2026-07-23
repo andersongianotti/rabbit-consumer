53e7a8f3ad12281182ea78db848ee7eadaea7bf6
Anderson Gianotti <anderson.gianotti@gmail.com>
2026-07-23 18:23:43 -0300

    Add Kafka producer to forward RabbitMQ messages

--- src/main/java/com/example/rabbitconsumer/consumer/RabbitMQConsumer.java
+++ src/main/java/com/example/rabbitconsumer/consumer/RabbitMQConsumer.java
@@ -1,16 +1,25 @@
 package com.example.rabbitconsumer.consumer;
 
 import com.example.rabbitconsumer.model.Message;
+import com.example.rabbitconsumer.producer.KafkaProducer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMQConsumer {
 
+    private final KafkaProducer kafkaProducer;
+
+    public RabbitMQConsumer(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
     @RabbitListener(queues = "jsonQueue")
     public void consumirMensagem(Message mensagem) {
         System.out.println("Mensagem recebida do RabbitMQ:");
         System.out.println("ID: " + mensagem.getMensagemId());
         System.out.println("Conteúdo: " + mensagem.getMessage());
+        
+        kafkaProducer.enviarMensagem(mensagem);
     }
 }
